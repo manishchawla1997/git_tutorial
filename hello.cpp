944c922a7edaf1bca201fd2ad18537d3c2f254a1
manishchawla1997 <32111866+manishchawla1997@users.noreply.github.com>
2017-09-19 14:58:51 -0700

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -4,6 +4,7 @@ using namespace std;
 	
 int main()
 {
-	cout << "Hello!" <<  endl;
-	cout << "Great, you managed to get a copy of the repository from GitHub" <<  endl;
+	cout << "Hey WHATSUP!!" <<  endl;
+	cout << "Good JOB...Awesome!!!" <<  endl;
+	return 0;
 }
